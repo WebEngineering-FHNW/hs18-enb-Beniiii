6ee9ea90a68fa2363b9fef271125c0f502f35d68
Benjamin @ Huber <benjamin.huber@students.fhnw.ch>
2018-12-15 21:36:09 +0100

    Added first integration test

---
+++ src/integration-test/groovy/mvc/InPlaceCalculatorSpec.groovy
@@ -0,0 +1,28 @@
+package mvc
+
+import geb.spock.GebSpec
+import grails.testing.mixin.integration.Integration
+
+/**
+ * See http://www.gebish.org/manual/current/ for more instructions
+ */
+@Integration
+class InPlaceCalculatorSpec extends GebSpec {
+
+    void "Calculate in place with a self-refreshing view"() {
+        when: "Go to start GSP page by calling it disguised as HTML"
+        go '/InPlaceCalculator.html'
+        then:
+        title == "Calculator"
+
+        when: "set valid input"
+        $("form").input1 = 18.0
+        $("form").input2 = 13.0
+        $("form").operator = "+"
+        $("input", type: "submit").click()
+
+        then: "Result is displayed"
+        $("output").text() == "31.0"
+
+    }
+}
