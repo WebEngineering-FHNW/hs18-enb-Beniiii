b071c7535de9e4b15bbbe0909c9038832752c0e6
Benjamin @ Huber <benjamin.huber@students.fhnw.ch>
2018-12-15 22:38:50 +0100

    Added unit tests for sub, mult and div

--- src/test/groovy/webec/InPlaceCalculatorControllerSpec.groovy
+++ src/test/groovy/webec/InPlaceCalculatorControllerSpec.groovy
@@ -22,4 +22,53 @@ class InPlaceCalculatorControllerSpec extends Specification implements Controlle
         25.0    | 19.2   | "+"      |"44.2"
         -13.0   | 25.0   | "+"      |"12.0"
     }
+
+    void "input1 minus input2 should equal result"(input1, input2, operator, result) {
+        when:
+        def model = new CalculatorModel(input1: input1, input2: input2, operator: operator)
+        controller.calc(model)
+        then: "add calculation"
+        model.result == result
+        where:
+        input1  | input2 | operator | result
+        0.0     | 0.0    | "-"      |"0.0"
+        5.5     | 0.0    | "-"      |"5.5"
+        0.0     | 13.7   | "-"      |"-13.7"
+        25.0    | 10.2   | "-"      |"14.8"
+        -13.0   | 25.0   | "-"      |"-38.0"
+        -13.0   | -13.0  | "-"      |"0.0"
+    }
+
+    void "input1 multiply with input2 should equal result"(input1, input2, operator, result) {
+        when:
+        def model = new CalculatorModel(input1: input1, input2: input2, operator: operator)
+        controller.calc(model)
+        then: "add calculation"
+        model.result == result
+        where:
+        input1  | input2 | operator | result
+        0.0     | 0.0    | "*"      |"0.0"
+        5.5     | 0.0    | "*"      |"0.0"
+        0.0     | 13.7   | "*"      |"0.0"
+        25.0    | 10.0   | "*"      |"250.0"
+        -10.0   | 25.0   | "*"      |"-250.0"
+        -13.0   | -10.0  | "*"      |"130.0"
+    }
+
+    void "input1 divided by input2 should equal result"(input1, input2, operator, result) {
+        when:
+        def model = new CalculatorModel(input1: input1, input2: input2, operator: operator)
+        controller.calc(model)
+        then: "add calculation"
+        model.result == result
+        where:
+        input1  | input2 | operator | result
+        0.0     | 0.0    | "/"      |"NaN"
+        5.5     | 0.0    | "/"      |"Infinity"
+        -5.5    | 0.0    | "/"      |"-Infinity"
+        0.0     | 13.7   | "/"      |"0.0"
+        25.0    | 10.0   | "/"      |"2.5"
+        -25.0   | 25.0   | "/"      |"-1.0"
+        -13.0   | -10.0  | "/"      |"1.3"
+    }
 }
